103afb1e7ddc8238ec9a2085e878d079c94f021d
coffeemania68 <gmschool0245@gmail.com>
2025-01-24 16:23:29 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,9 +4,54 @@ const holidayPrograms = {
             { time: "21:45", title: "달짝지근해", type: "영화" },
             { time: "23:30", title: "룸쉐어링", type: "영화" }
         ],
-        // ... (기존 데이터 유지)
+                "MBC": [
+            { time: "10:45", title: "리바운드", type: "영화" },
+            { time: "23:40", title: "귀공자", type: "영화" }
+        ],
+        "SBS": [
+            { time: "17:15", title: "콘크리트 유토피아", type: "영화" }
+        ],
+        "CABLE": [
+            { time: "22:10", title: "올빼미", channel: "JTBC", type: "영화" }
+        ]
     },
-    // ... (다른 날짜 데이터 유지)
+    "2025-01-28": {
+        "KBS": [
+            { time: "20:45", title: "탈주", type: "영화" },
+            { time: "22:15", title: "데시벨", type: "영화" }
+        ],
+        "SBS": [
+            { time: "22:10", title: "서울의 봄", type: "영화" }
+        ]
+    },
+    "2025-01-29": {
+        "KBS": [
+            { time: "21:00", title: "파묘", type: "영화" },
+            { time: "23:20", title: "세자매", type: "영화" }
+        ],
+        "CABLE": [
+            { time: "18:40", title: "정동원 성탄총동원 콘서트", channel: "ENA", type: "공연" },
+            { time: "18:50", title: "임영웅 아임 히어로 더 스타디움", channel: "TV조선", type: "공연" }
+        ]
+    },
+    "2025-01-30": {
+        "KBS": [
+            { time: "12:10", title: "멍뭉이", type: "영화" },
+            { time: "21:50", title: "시민덕희", type: "영화" }
+        ],
+        "MBC": [
+            { time: "23:40", title: "싱글 인 서울", type: "영화" }
+        ],
+        "SBS": [
+            { time: "12:20", title: "스위치", type: "영화" },
+            { time: "22:10", title: "성시경 자 오늘은", type: "공연" }
+        ],
+        "CABLE": [
+            { time: "19:50", title: "빅토리", channel: "JTBC", type: "영화" },
+            { time: "20:10", title: "드림", channel: "JTBC", type: "영화" },
+            { time: "21:10", title: "비공식작전", channel: "TV조선", type: "영화" }
+        ]
+    }
 };
 
 const BROADCAST_LINKS = {
